cc5baff9036d7a18a28962b49cb17afcc6c248b9
furaga <furaga.fukahori@gmail.com>
2022-11-28 11:42:10 +0900

    use digit_ocr

--- python/auto_score_counter.py
+++ python/auto_score_counter.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from playsound import playsound
 import pyocr
 from PIL import Image, ImageEnhance
+from lib import digit_ocr
 
 
 def parse_args():
@@ -18,18 +19,6 @@ def parse_args():
     parser.add_argument('--my_tag', type=str, default="RR")
     args = parser.parse_args()
     return args
-
-
-def pil2cv(image):
-    ''' PIL型 -> OpenCV型 '''
-    new_image = np.array(image, dtype=np.uint8)
-    if new_image.ndim == 2:  # モノクロ
-        pass
-    elif new_image.shape[2] == 3:  # カラー
-        new_image = new_image[:, :, ::-1]
-    elif new_image.shape[2] == 4:  # 透過
-        new_image = new_image[:, :, [2, 1, 0, 3]]
-    return new_image
 
 
 def cv2pil(image):
@@ -43,85 +32,6 @@ def cv2pil(image):
         new_image = cv2.cvtColor(new_image, cv2.COLOR_BGRA2RGBA)
     new_image = Image.fromarray(new_image)
     return new_image
-
-
-def replace_chars(text):
-    list_of_numbers = re.findall(r'\d+', text)
-    result_number = ''.join(list_of_numbers)
-    return result_number
-
-
-def detect_name_regions(img):
-    _, thr = cv2.threshold(img, 175, 255, cv2.THRESH_BINARY)
-
-    kernel = np.ones((5, 5), np.uint8)
-    closed = cv2.morphologyEx(thr, cv2.MORPH_CLOSE, kernel)
-    kernel = np.ones((3, 3), np.uint8)
-    closed = cv2.dilate(closed, kernel, iterations=1)
-
-    contours, hierarchy = cv2.findContours(
-        closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-    )
-    t_bounding_boxes = [cv2.boundingRect(c) for c in contours]
-    bounding_boxes = []
-    for x, y, w, h in t_bounding_boxes:
-        if 100 < x < 200 and x + w < 360 and 10 < h < 40:
-            merged = False
-            for i, b in enumerate(bounding_boxes):
-                if abs(b[1] - y) < 5:
-                    bounding_boxes[i] = [
-                        min(bounding_boxes[i][0], x),
-                        min(bounding_boxes[i][1], y),
-                        max(bounding_boxes[i][0] +
-                            bounding_boxes[i][2], x + w),
-                        max(bounding_boxes[i][1] +
-                            bounding_boxes[i][3], y + h),
-                    ]
-                    bounding_boxes[i][2] -= bounding_boxes[i][0]
-                    bounding_boxes[i][3] -= bounding_boxes[i][1]
-                    merged = True
-                    break
-            if not merged:
-                bounding_boxes.append((x, y, w, h))
-
-    return bounding_boxes
-
-
-def create_ocr():
-    tools = pyocr.get_available_tools()
-    tool = tools[0]
-    builder = pyocr.builders.TextBuilder(tesseract_layout=6)
-    return tool, builder
-
-
-def read_names(img, bounding_boxes, tool, builder, thresh):
-    _, thr = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
-    img_pil = cv2pil(thr)
-    bounding_boxes = sorted(bounding_boxes, key=lambda b: b[1])
-    names = []
-    for i, (x, y, w, h) in enumerate(bounding_boxes):
-        margin = 3
-        crop = img_pil.crop(
-            [x - margin, y - margin, x + w + margin, y + h + margin])
-        # crop.save(f"{i:03d}.png")
-        name = tool.image_to_string(crop, lang='eng', builder=builder)
-        names.append(name)
-    return names
-
-
-def read_scores(img, bounding_boxes, tool, builder, thresh):
-    _, thr = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
-    img_pil = cv2pil(thr)
-    bounding_boxes = sorted(bounding_boxes, key=lambda b: b[1])
-    scores = []
-    for i, (x, y, w, h) in enumerate(bounding_boxes):
-        margin = 10
-        crop = img_pil.crop(
-            [img_pil.width - 45, y - margin, img_pil.width, y + h + margin])
-        # crop.save(f"s{i:03d}.png")
-        score = tool.image_to_string(crop, lang='ssd', builder=builder)
-        scores.append(score)
-    return scores
 
 
 def common_prefix(n1, n2):
@@ -158,12 +68,12 @@ def estimate_tags(names):
                 prefix = common_prefix(n, n2)
                 suffix = common_suffix(n, n2)
                 if len(prefix) > 0:
-                    new_tag = prefix, True
+                    if len(new_tag) <= 0 or len(new_tag[0]) < len(prefix):
+                        new_tag = prefix, True
                 elif len(suffix) > 0:
-                    # prefixが登録済みならむし
-                    if len(new_tag) > 0 and new_tag[1]:
-                        continue
-                    new_tag = suffix, False
+                    if len(new_tag) <= 0 or len(new_tag[0]) < len(suffix):
+                        new_tag = suffix, False
+
         if len(new_tag) <= 0:
             print(f"tag for {n} not found. Use its name as tag.")
             tags.append((n, True))
@@ -348,21 +258,28 @@ def correct_scores(scores, score_regions, img_bgr):
             scores[i] = ""
         prev_score = val
 
-    margin = 10
+    margin = 8
+    img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
     for i in range(len(scores)):
         if scores[i] != "":
             continue
         x, y, w, h = score_regions[i]
+        if w < h:
+            w += h
+            x -= h
         top = max(0, y-margin)
         bottom = min(y+h+margin, img_bgr.shape[0])
         left = max(0, x-margin)
         right = min(x+w+margin, img_bgr.shape[1])
-        crop = img_bgr[top:bottom, left:right]
+        crop = img_gray[top:bottom, left:right]
 
-        # TODO: 数字はGoogle OCRで認識しづらい。文字は0~9しかないのでここだけテンプレートマッチしてみる？
-
-        import uuid
         cv2.imwrite(f"data/scores_crop/{i}.png", crop)
+
+        # デジタル数字はGoogle OCRで認識しづらい。がんばって自力で認識する
+        score = digit_ocr.detect_digit(crop)
+        print("digit_ocr: score=", score)
+
+        scores[i] = str(score)
 
     return scores
 
@@ -389,6 +306,37 @@ def main(args):
     img_bgr = cv2.imread(str(args.img_path))
 
     scores = correct_scores(scores, score_regions, img_bgr)
+    for n, s in zip(names, scores):
+        print(n, ":", s)
+
+    # タグを推定
+    tags = estimate_tags(names)
+
+    def has_tag(n, t):
+        if t[1] and n.startswith(t[0]):
+            return True
+        if not t[1] and n.endswith(t[0]):
+            return True
+        return False
+
+    def to_int(s):
+        try:
+            return int(s)
+        except Exception:
+            return 0
+
+    tag_scores = {t[0]: np.sum([to_int(s) for n, s in zip(
+        names, scores) if has_tag(n, t)]) for t in tags}
+
+    # my_tagとの差分付きでスコア大きい順に表示
+    my_tag = [t for t, _ in tags if args.my_tag in t]
+    my_tag = my_tag[0] if len(my_tag) >= 1 else tags[0][0]
+    sorted_tag_scores = sorted(tag_scores.items(), key=lambda ts: -ts[1])
+    print("====================")
+    for tag, score in sorted_tag_scores:
+        diff = score - tag_scores[my_tag]
+        print(f"{tag}: {score} ({'+' if diff >= 0 else ''}{diff})")
+    print("====================")
 
     for x, y, w, h in all_bounding_boxes:
         cv2.rectangle(img_bgr, (x, y), (x + w, y + h), (0, 0, 255), 2, -1)
@@ -397,66 +345,8 @@ def main(args):
     for x, y, w, h in score_regions:
         cv2.rectangle(img_bgr, (x, y), (x + w, y + h), (0, 255, 0), 4, -1)
 
-    #cv2.imshow("img_bgr", img_bgr)
-   # cv2.waitKey(0)
-    #exit()
-
-    # img = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
-
-    # # OCRエンジン取得
-    # tool, builder = create_ocr()
-
-    # # 自分以外のプレイヤ名・スコア
-    # bounding_boxes = detect_name_regions(img)
-    # names = read_names(img, bounding_boxes, tool, builder, 175)
-    # scores = read_scores(img, bounding_boxes, tool, builder, 175)
-
-    # # 自分のプレイヤ名・スコア
-    # img2 = 255 - img
-    # bounding_boxes2 = detect_name_regions(img2)
-    # names2 = read_names(img2, bounding_boxes2, tool, builder, 100)
-    # scores2 = read_scores(img2, bounding_boxes2, tool, builder, 100)
-
-    # # タグを推定
-    # names += names2
-    # scores += scores2
-    # tags = estimate_tags(names)
-
-    # print("names", names)
-    # print("scores", scores)
-    # print("tags", tags)
-
-    # # for x, y, w, h in bounding_boxes2:
-    # #     cv2.rectangle(img_bgr, (x, y), (x + w, y + h), (0, 0, 255), 2, -1)
-    # # cv2.imshow("img_bgr", img_bgr)
-    # # cv2.imshow("img2", img2)
-    # # cv2.waitKey(0)
-    # # exit()
-
-    # def has_tag(n, t):
-    #     if t[1] and n.startswith(t[0]):
-    #         return True
-    #     if not t[1] and n.endswith(t[0]):
-    #         return True
-    #     return False
-
-    # def to_int(s):
-    #     try:
-    #         return int(s)
-    #     except Exception:
-    #         return 0
-
-    # tag_scores = {t[0]: np.sum([to_int(s) for n, s in zip(
-    #     names, scores) if has_tag(n, t)]) for t in tags}
-
-    # # my_tagとの差分付きでスコア大きい順に表示
-    # my_tag = [t for t, _ in tags if args.my_tag in t][0]
-    # sorted_tag_scores = sorted(tag_scores.items(), key=lambda ts: -ts[1])
-    # print("====================")
-    # for tag, score in sorted_tag_scores:
-    #     diff = score - tag_scores[my_tag]
-    #     print(f"{tag}: {score} ({'+' if diff >= 0 else ''}{diff})")
-    # print("====================")
+    cv2.imshow("img_bgr", img_bgr)
+    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
---
+++ python/lib/digit_ocr.py
@@ -0,0 +1,198 @@
+import cv2
+import numpy as np
+
+MIN_ROD_CONTRAST = 32
+
+
+def is_digit_or_segment(roi):
+    top, left, right, bottom, area = roi
+    height, width = bottom + 1 - top, right + 1 - left
+    h_per_w = height / width
+    if area / (height * width) > .99 or area / (height * width) < .1:
+        return False
+    if area / (height * width) > .85 and .5 <= h_per_w <= 2:
+        return False
+    if h_per_w < .3 or h_per_w > 10:
+        return False
+    return True
+
+
+def rod_thresholding(roi, image_height, image_width, min_area_ratio=.0025, max_area_ratio=.2,
+                     min_height_per_width=.5, max_height_per_width=10,
+                     min_height_ratio=.15):
+    top, left, right, bottom, area = roi
+    image_area = image_height * image_width
+    if (bottom + 1 - top) / image_height <= min_height_ratio:
+        if area / image_area <= min_area_ratio:
+            return False
+        if area / image_area >= max_area_ratio:
+            return False
+    h_per_w = (bottom + 1 - top) / (right + 1 - left)
+    if h_per_w <= min_height_per_width:
+        return False
+    if h_per_w >= max_height_per_width:
+        return False
+    if h_per_w <= 4 and area / ((bottom + 1 - top) * (right + 1 - left)) > .85:
+        return False
+    return True
+
+
+SEGMENTS2DIGIT = {0b0000000: -1, 0b0000001: -1, 0b0000010: 1, 0b0000011: -1, 0b0000100: -1,
+                  0b0000101: -1, 0b0000110: -1, 0b0000111: -1, 0b0001000: -1, 0b0001001: -1,
+                  0b0001010: -1, 0b0001011: -1, 0b0001100: -1, 0b0001101: -1, 0b0001110: -1,
+                  0b0001111: -1, 0b0010000: 1, 0b0010001: -1, 0b0010010: 1, 0b0010011: -1,
+                  0b0010100: -1, 0b0010101: -1, 0b0010110: -1, 0b0010111: -1, 0b0011000: -1,
+                  0b0011001: -1, 0b0011010: 4, 0b0011011: 3, 0b0011100: -1, 0b0011101: 2,
+                  0b0011110: -1, 0b0011111: -1, 0b0100000: -1, 0b0100001: -1, 0b0100010: -1,
+                  0b0100011: -1, 0b0100100: -1, 0b0100101: -1, 0b0100110: -1, 0b0100111: -1,
+                  0b0101000: -1, 0b0101001: -1, 0b0101010: 4, 0b0101011: 5, 0b0101100: -1,
+                  0b0101101: -1, 0b0101110: -1, 0b0101111: 6, 0b0110000: -1, 0b0110001: -1,
+                  0b0110010: 4, 0b0110011: -1, 0b0110100: -1, 0b0110101: -1, 0b0110110: -1,
+                  0b0110111: 0, 0b0111000: 4, 0b0111001: -1, 0b0111010: 4, 0b0111011: 9,
+                  0b0111100: -1, 0b0111101: -1, 0b0111110: -1, 0b0111111: 8, 0b1000000: -1,
+                  0b1000001: -1, 0b1000010: -1, 0b1000011: -1, 0b1000100: -1, 0b1000101: -1,
+                  0b1000110: -1, 0b1000111: -1, 0b1001000: -1, 0b1001001: -1, 0b1001010: -1,
+                  0b1001011: 3, 0b1001100: -1, 0b1001101: -1, 0b1001110: -1, 0b1001111: 6,
+                  0b1010000: -1, 0b1010001: -1, 0b1010010: 7, 0b1010011: 3, 0b1010100: -1,
+                  0b1010101: 2, 0b1010110: -1, 0b1010111: 0, 0b1011000: -1, 0b1011001: 2,
+                  0b1011010: 3, 0b1011011: 3, 0b1011100: 2, 0b1011101: 2, 0b1011110: -1,
+                  0b1011111: 8, 0b1100000: -1, 0b1100001: -1, 0b1100010: 7, 0b1100011: 5,
+                  0b1100100: -1, 0b1100101: -1, 0b1100110: -1, 0b1100111: 0, 0b1101000: -1,
+                  0b1101001: 5, 0b1101010: 5, 0b1101011: 5, 0b1101100: -1, 0b1101101: 6,
+                  0b1101110: 6, 0b1101111: 6, 0b1110000: 7, 0b1110001: -1, 0b1110010: 7,
+                  0b1110011: 0, 0b1110100: -1, 0b1110101: 0, 0b1110110: 0, 0b1110111: 0,
+                  0b1111000: -1, 0b1111001: 9, 0b1111010: 9, 0b1111011: 9, 0b1111100: -1,
+                  0b1111101: 8, 0b1111110: 8, 0b1111111: 8}
+
+
+def ison(roi_bin, is_vertical: bool, ret_val: int) -> int:
+    if is_vertical:
+        thresh = roi_bin.shape[0] * .6
+        if (np.count_nonzero(roi_bin, axis=0) > thresh).any():
+            return ret_val
+    else:
+        thresh = roi_bin.shape[1] * .7
+        if (np.count_nonzero(roi_bin, axis=1) > thresh).any():
+            return ret_val
+    return 0
+
+
+def rod2digit(rod, rod_bin):
+    height_rod, width_rod = rod_bin.shape
+    _, labels, stats = cv2.connectedComponentsWithStats(rod_bin)[:3]
+    # Remove tiny blobs.
+    for i, stat in enumerate(stats):
+        left, top, w, h = stat[:4]
+        if max(w, h) < min(width_rod, height_rod) * .15:
+            rod_bin[i == labels] = 0
+
+    if height_rod / width_rod > 3:
+        read_digit = 1
+    else:
+        vline1, vline2 = width_rod // 3, width_rod * 2 // 3
+        hline1, hline2, hline3 = height_rod // 3, height_rod // 2, height_rod * 2 // 3
+        segments = [rod_bin[:hline1, vline1:vline2], rod_bin[:hline2, :vline1], rod_bin[:hline2, vline2:],
+                    rod_bin[hline1:hline3, vline1:vline2], rod_bin[hline2:,
+                                                                   :vline1], rod_bin[hline2:, vline2:],
+                    rod_bin[hline3:, vline1:vline2]]
+        is_verticals = [False, True, True, False, True, True, False]
+        ret_vals = [64, 32, 16, 8, 4, 2, 1]
+        read_digit = SEGMENTS2DIGIT[sum(
+            list(map(ison, segments, is_verticals, ret_vals)))]
+    if -1 == read_digit and np.max(np.count_nonzero(rod_bin, axis=0)) / max(1, np.max(np.count_nonzero(rod_bin, axis=1))) > 3.5:
+        read_digit = 1
+    return {'digit': int(read_digit), 'left': int(rod[1]), 'top': int(rod[0]),
+            'width': int(width_rod), 'height': int(height_rod)}
+
+
+def detect_digit(roi_gray):
+    KERNEL_4N = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], dtype=np.uint8)
+    roi_gray = roi_gray.copy()
+    roi_gray_enhanced = cv2.createCLAHE(
+        clipLimit=2.0, tileGridSize=(8, 8)).apply(roi_gray)
+    height, width = roi_gray_enhanced.shape
+    EDGELEN_PERCENT = (height + width) / 200
+
+    rods = []  # ROD = Region of a Digit
+
+    # Remove shadows near the display contour.
+    inner_x1, inner_x2 = int(width * .02), int(width * .98)
+    inner_y1, inner_y2 = int(height * .05), int(height * .95)
+    roi_bin_inner = cv2.adaptiveThreshold(roi_gray_enhanced[inner_y1:inner_y2, inner_x1:inner_x2], 255,
+                                          cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
+                                          max(int(min(height, width) / 2 * .5), 1) * 2 + 1, 2)
+    digits_brightness = np.mean(
+        roi_gray_enhanced[inner_y1:inner_y2, inner_x1:inner_x2][roi_bin_inner > 127])
+    roi_bin = cv2.threshold(roi_gray_enhanced, 255,
+                            digits_brightness, cv2.THRESH_BINARY)[1]
+    roi_bin[inner_y1:inner_y2, inner_x1:inner_x2] = roi_bin_inner.copy()
+
+    tmp = cv2.erode(roi_bin.copy(), KERNEL_4N,
+                    iterations=int(EDGELEN_PERCENT / 4 * .5) + 1)
+    _, labels, stats = cv2.connectedComponentsWithStats(tmp)[:3]
+    for i, stat in enumerate(stats):
+        if 0 == min(stat[0], stat[1]) or width == stat[0] + stat[2] or height == stat[1] + stat[3]:
+            roi_bin[i == labels] = 0
+
+    # Cut off bright area near the display contour and long bright line.
+    tmp = roi_bin.copy()
+    for i, bright in enumerate(np.count_nonzero(roi_bin, axis=1)):
+        if bright < width * .8:
+            continue
+        tmp[i, :] = 0
+    for i, bright in enumerate(np.count_nonzero(roi_bin, axis=0)):
+        if bright < height * .95:
+            continue
+        tmp[:, i] = 0
+    roi_bin = tmp.copy()
+
+    # Remove blobs which is not a segment or a digit.
+    labels, stats = cv2.connectedComponentsWithStats(roi_bin)[1:3]
+    for i, stat in enumerate(stats):
+        left, top, w, h = stat[:4]
+        if roi_bin[i == labels][0] < 128:
+            continue
+        right, bottom = left + w, top + h
+        if not is_digit_or_segment((top, left, right, bottom, stat[4])):
+            roi_bin[i == labels] = 0
+
+    # Remove thin lines.
+
+    kernel = np.ones((5, 5), np.uint8)
+    roi_bin = cv2.morphologyEx(roi_bin, cv2.MORPH_CLOSE, kernel)
+    # roi_bin = cv2.erode(roi_bin, KERNEL_4N, iterations=int(EDGELEN_PERCENT / 2 * .5) + 1)
+    # roi_bin = cv2.dilate(roi_bin, KERNEL_4N, iterations=int(EDGELEN_PERCENT / 2 * .5) + 1)
+
+    n_labels, labels, stats = cv2.connectedComponentsWithStats(roi_bin)[:3]
+    json_digits = {'digits': []}
+
+    for i, stat in enumerate(stats):
+        left, top, w, h = stat[:4]
+
+        if h < height * 0.5 or height * 0.99 < h:
+            continue
+
+        right, bottom = left + w - 1, top + h - 1
+        if not rod_thresholding((top, left, right, bottom, stat[4]), height, width, max_area_ratio=.4):
+            continue
+
+        # Remove low contrast area. (ex. shadow but not a digit)
+        rod_gray = roi_gray[top:bottom+1, left:right+1].copy()
+        height_rod, width_rod = rod_gray.shape
+        nth = width_rod * \
+            height_rod // (40 if height_rod / width_rod > 3 else 12)
+        min_value = np.partition(rod_gray.ravel(), nth)[nth]
+        rod_gray[(i != labels)[top:bottom+1, left:right+1]] = min_value
+        if np.max(rod_gray) - min_value < MIN_ROD_CONTRAST:
+            continue
+
+        rods.append((left, top, left + w, top + h, None))
+
+    rods = sorted(rods, key=lambda r: r[0])
+
+    num = 0
+    for rod in rods:
+        left, top, right, bottom, _ = rod
+        digit = rod2digit(rod, roi_bin[top:bottom, left:right])["digit"]
+        num = 10 * num + digit
+    return num
